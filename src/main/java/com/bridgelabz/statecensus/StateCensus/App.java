3a95993e0883371e13c508d536a6d96fb6647e1d
lalit-003 <gahlawatl7@gmail.com>
2020-10-19 00:49:37 +0530

    Start commit

---
+++ src/main/java/com/bridgelabz/statecensus/StateCensus/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.statecensus.StateCensus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to state analyzr program" );
+    }
+}
