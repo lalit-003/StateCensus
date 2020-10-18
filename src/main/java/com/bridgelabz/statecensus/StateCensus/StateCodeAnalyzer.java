436f27fab3f27c164d5e7eafaa3ef08e518eb534
lalit-003 <gahlawatl7@gmail.com>
2020-10-19 03:02:05 +0530

    Updated functionality to throw custom exception when provided wrong file path

--- src/main/java/com/bridgelabz/statecensus/StateCensus/StateCodeAnalyzer.java
+++ src/main/java/com/bridgelabz/statecensus/StateCensus/StateCodeAnalyzer.java
@@ -1,28 +1,35 @@
 package com.bridgelabz.statecensus.StateCensus;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.logging.Logger;
+import com.bridgelabz.statecensus.StateCensus.ExceptionStateCensus.CensusExceptionType;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class StateCodeAnalyzer {
-	public static int readingStateCensusFromCSV(String filePath)
+	public static int readingStateCensusFromCSV(String filePath) throws ExceptionStateCensus
+
 	{
 		Logger logger = Logger.getLogger(StateCensusAnalyzer.class.getName());
 		Reader reader = null;
 		try {
               reader = Files.newBufferedReader(Paths.get(filePath));		
 		}
-		catch(Exception exception)
+		catch(NoSuchFileException e)
 		{
 			logger.warning("Not able to open file at specified path");
-			return 0;
+		   throw new ExceptionStateCensus(CensusExceptionType.WRONG_FILE_NAME, "no such file exists at given file location");
 		}
-       CsvToBean<CSVStateCode> csvToBean = new CsvToBeanBuilder<CSVStateCode>(reader)
+		catch(IOException e)
+		{
+			throw new ExceptionStateCensus(CensusExceptionType.OTHER_IO_EXCEPTION, "some other file exception other than wrong name");
+		}       CsvToBean<CSVStateCode> csvToBean = new CsvToBeanBuilder<CSVStateCode>(reader)
                 .withType(CSVStateCode.class)
                 .withIgnoreLeadingWhiteSpace(true)
                 .build();	
