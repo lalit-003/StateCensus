63bdfe62879ced02f539f735219e86c34c12b4f1
lalit-003 <gahlawatl7@gmail.com>
2020-10-21 00:41:27 +0530

    Refactored code to add CSVException

--- src/main/java/com/bridgelabz/statecensus/StateCensus/ICSVBuilder.java
+++ src/main/java/com/bridgelabz/statecensus/StateCensus/ICSVBuilder.java
@@ -3,8 +3,10 @@ package com.bridgelabz.statecensus.StateCensus;
 import java.io.Reader;
 import java.util.Iterator;
 
+import com.opencsv.exceptions.CsvException;
+
 public  interface ICSVBuilder<E> {
 	
-		public <E> Iterator<E> getIterator(Reader reader, Class className) throws ExceptionStateCensus;
+		public <E> Iterator<E> getIterator(Reader reader, Class className) throws CsvException;
 	}
 
